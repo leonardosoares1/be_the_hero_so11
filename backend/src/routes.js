42d8e5f4a23e341f0f367521cc0e28b3d1a51624
leonardosoares1 <dev@doleonardo.com.br>
2020-03-29 09:30:13 -0300

    feat: add tests and validators

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { celebrate, Segments, Joi } = require('celebrate')
 
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
@@ -7,16 +8,52 @@ const SessionController = require('./controllers/SessionController')
 
 const routes = express.Router()
 
-routes.get('/ongs/:id/incidents',  OngIncidentController.index)
+routes.get('/ongs/:id/incidents', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required()
+  }).unknown()
+}), OngIncidentController.index)
 
-routes.get('/ongs',  OngController.index)
-routes.post('/ongs',  OngController.store)
+routes.get('/ongs', OngController.index)
 
-routes.get('/incidents',  IncidentController.index)
-routes.post('/incidents',  IncidentController.store)
-routes.delete('/incidents/:id',  IncidentController.destroy)
+routes.post('/ongs', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.string().required().min(10).max(11),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2)
+  })
+}), OngController.store)
 
-routes.post('/sessions',  SessionController.store)
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number()
+  })
+}), IncidentController.index)
+
+routes.post('/incidents', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required()
+  }).unknown(),
+  [Segments.BODY]: Joi.object().keys({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    value: Joi.number().required(),
+  })
+}), IncidentController.store)
+
+routes.delete('/incidents/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required()
+  })
+}), IncidentController.destroy)
+
+routes.post('/sessions', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    id: Joi.string().required(),
+  })
+}), SessionController.store)
 
 
 module.exports = routes
